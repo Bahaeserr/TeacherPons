714f43162b589803ceb5e5eee36602f0fd473112
bahae <115068298+Bahaeserr@users.noreply.github.com>
2022-10-24 19:52:25 +0200

    commit  1

--- Bahae1.py
+++ Bahae1.py
@@ -8,7 +8,7 @@ class Point3D:
         self.y=y
         self.z=z
 
-#Commit 1: __init__(constructor), setters, getters and __str__ (tostring)
+    #Commit 1: __init__(constructor), setters, getters and __str__ (tostring)
 
     def get_X(self):
         return self.x
@@ -31,15 +31,26 @@ class Point3D:
     def __str__(self):
         return "X:"+str(self.x)+" Y:"+str(self.y)+" Z:"+str(self.z)
 
-#Commit 2: Distance to origin.
-  def distance_to_origin(self):
 
-#Commit 3: Distance between 2 points.
- def calculate_distance(self, point_2):
+    #Commit 2: Distance to origin.
+    def distance_to_origin(self):
+        pass
 
-#Commit 4: Determine quadrant
+
+    #Commit 3: Distance between 2 points.
+    def calculate_distance(self, point_2):
+        pass
+
+    #Commit 4: Determine quadrant
     def calculate_quadrant(self):
+        #Devuelve 0 si está en el origen de coordenadas o sobre alguno de los ejes.
+        #Devuelve 1 si está en el primer cuadrante (x e y positivos).
+        #Devuelve 2 si está en el segundo cuadrante (x negativo e y positivo).
+        #Devuelve 3 si está en el tercer cuadrante (x e y negativos).
+        #Devuelve 4 si está en el cuarto cuadrante (x positivo e y negativo).
+        pass
 
-#Commit 5: Given a list of Points, determine which of them is closer to *self*
+
+    #Commit 5: Given a list of Points, determine which of them is closer to *self*
     def get_closest_point(self, points):
         pass
